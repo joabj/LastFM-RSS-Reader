88dbe29d8addcffb5a9d6b190655fa5c55be2220
joabj <home@joabj.com>
2017-06-17 16:04:37 -0400

    Update Environs.py

--- Environs.py
+++ Environs.py
@@ -1,10 +1,10 @@
 #Store the environmental variables here for the WeeklyRoundup.py program
 
 #LastFM User name
-User = '[FILL IN LAST.FM USER NAME HERE'
+User = '[FILL IN LAST.FM USER NAME HERE]'
 
 #LastFM personal keys
 PersonalKey='[FILL IN LAST.FM PERSONAL KEY HERE]'
 
 #File to write the data in
-WriteFile = '[FILL IN FULL PATH OF THE TXT FILE TO WRITE DATA HERE'
+WriteFile = '[FILL IN FULL PATH OF THE TXT FILE TO WRITE DATA HERE]'
