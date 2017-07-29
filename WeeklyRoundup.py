8814c5ed1e138e1c166abcba35ce65a3ffeb4342
joabj <home@joabj.com>
2017-07-29 15:40:51 -0400

    Update WeeklyRoundup.py

--- WeeklyRoundup.py
+++ WeeklyRoundup.py
@@ -1,9 +1,14 @@
+#This program pulls the top albums, artists and tracks captured by LastFM in the json format (run "pprint" for json taxonomy)
+#It requires the WeeklyRoundup.html file to complete the formatting.
+
+import datetime
 import json
 import requests
 from sys import argv
 from pprint import pprint
-#The small library with the enviornmental settings...
+#The small library with the environmental settings...
 import Environs
+
 
 #Some API boilerplate we'll need soon
 RequestPrefix='http://ws.audioscrobbler.com/2.0/?method='
@@ -35,8 +40,9 @@ while x < 10:
 	ArtistNameString = "<td><div class=\"album\"><b>" + ArtistName + "</b></div></td>"
 	ArtistPlaycountString = "<td><div class=\"Playcount\">" + ArtistPlaycount + "</div></td></tr>"
 	
+	
 	target.write(Number)
-	target.write(ArtistNameString)
+	target.write(ArtistNameString.encode('ascii', 'ignore'))
 	target.write("\n")
 	target.write(ArtistPlaycountString)
 	target.write("\n")
@@ -62,7 +68,7 @@ ChartPlacement = 1
 target.write("<h3>Top Albums</h3>")
 target.write("<table><tr><th><b>#</b></th><th>Name</th><th>Artist</th></tr>")
 
-while x < 20:
+while x < 10:
 	AlbumName =  data[u'weeklyalbumchart'][u'album'][x][u'name']
 	AlbumPlaycount = data[u'weeklyalbumchart'][u'album'][x][u'playcount']
 	if AlbumName.find("SXSW") == -1:
@@ -75,7 +81,7 @@ while x < 20:
 	AlbumArtistString = "<td><div class=\"artist\">" + AlbumArtist + "</div></td></tr>"
 		
 	target.write(Number)
-	target.write(AlbumNameString)
+	target.write(AlbumNameString.encode('ascii', 'ignore'))
 	target.write("\n")
 	target.write(AlbumArtistString.encode('ascii', 'ignore'))
 	target.write("\n")
@@ -87,7 +93,7 @@ while x < 20:
 target.write("</table>")
 target.write("<img id=ImageCenter src=/Tilde-Color.jpg  height=27 width=60><p>")	
 
-#Top 10 Tracks
+#Top 5 Tracks
 RequestService='user.getweeklytrackchart'
 AssembledRequest = RequestPrefix+RequestService+RequestServiceUser+RequestPersonalKey+RequestSuffix
 response = requests.get(AssembledRequest)
@@ -98,18 +104,20 @@ ChartPlacement = 1
 target.write("<h3>Top Tracks</h3>")
 target.write("<table><tr><th><b>#</b></th><th>Song</th><th>Artist</th><th>Times Played</th></tr>")
 
-while x < 10:
+while x < 5:
 	TrackName =  data[u'weeklytrackchart'][u'track'][x][u'name']
 	TrackArtist =  data[u'weeklytrackchart'][u'track'][x][u'artist'][u'#text']
 	TrackPlaycount = data[u'weeklytrackchart'][u'track'][x][u'playcount']
+
 		
 	Number = "<tr><td>#" + str(ChartPlacement) + "   </div></td>"
 	AlbumNameString = "<td><div class=\"album\"><b>" + TrackName + "</b></div></td>"
 	AlbumArtistString = "<td><div class=\"artist\">" + TrackArtist + "</div></td>"
 	AlbumPlaycountString = "<td><div class=\"Playcount\">" + TrackPlaycount + "</div></td></tr>"
-
+	
+	
 	target.write(Number)
-	target.write(AlbumNameString)
+	target.write(AlbumNameString.encode('ascii', 'ignore'))
 	target.write("\n")
 	target.write(AlbumArtistString.encode('ascii', 'ignore'))
 	target.write("\n")
@@ -122,21 +130,10 @@ while x < 10:
 
 target.write("</table></center>")	
 target.write("<img id=ImageCenter src=/Tilde-Color.jpg  height=27 width=60><p>")	
-	
-
 target.close()
+
+
+
 
 #For printing the JSON taxonomy
 #pprint(data)
-
-
-#Docs
-#https://pythonspot.com/en/json-encoding-and-decoding-with-python/
-#http://www.last.fm/api/show/user.getWeeklyTrackChart
-#http://www.last.fm/api/show/user.getWeeklyArtistChart
-#http://www.last.fm/api/show/user.getWeeklyAlbumChart
-#http://stackoverflow.com/questions/5141559/unicodeencodeerror-ascii-codec-cant-encode-character-u-xef-in-position-0
-#http://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method
-#http://stackoverflow.com/questions/7361253/how-to-determine-whether-a-substring-is-in-a-different-string
-#http://www.last.fm/api/show/user.getTopAlbums
-#http://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method
